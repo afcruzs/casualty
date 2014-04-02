f9cd66f2b51a0509d2612e353ced8411028a74ef
Diego Alejandro Vanegas Prada <davanegaspr@unal.edu.co>
2014-04-01 22:11:30 -0500

    se cre? la clase group con sus respectivos m?todos, falto lo del cambio de member que es una hashmap a un treemap, se dej? un mensaje en render para que muestre algo al menos cuando entren a index y una parte de las pruebas quedaron comenta riadas por si tienen alguna sugerencia al respecto

--- grails-app/controllers/com/casualtyApp/GroupController.groovy
+++ grails-app/controllers/com/casualtyApp/GroupController.groovy
@@ -1,5 +1,11 @@
 package com.casualtyApp
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.casualtyApp.model.Group
+import com.casualtyApp.model.Event
+
 
 /*
  * 	This controller handles CRUD operations on Group.
@@ -8,9 +14,27 @@ package com.casualtyApp
 
 class GroupController {
 
-    def index() { }
+    def index() { 
+		
+		def g = new Group();
+		
+		//def h = new Event(2, "dcbsh", Date startTime,
+			//Date endTime, "chdsvc", 3, ArrayList<String> tags,
+			//3.5, 5.6, 435);
+		//def i = new Event();
+		//def j = new Event();
+		
+		//g.addEvent(h);
+		//g.addEvent(i);
+		//g.addEvent(j);
+		render g 
 	
-	def create() { }
+	}
+	
+	def create() {
+		
+		
+	}
 	
 	def read() { }
 	
--- grails-app/domain/com/casualtyApp/model/Group.groovy
+++ grails-app/domain/com/casualtyApp/model/Group.groovy
@@ -1,10 +1,75 @@
 package com.casualtyApp.model
 
-class Group {
+class Group{
 	/*
-	 * Here goes Diego's Work!!!
 	 * 
-	 * @author: Felipe
+	 * @author: Diego
 	 */
-	def idgroup = 1	
+	def int idGroup
+	def String nameGroup
+	def String description
+	def Date createAt
+	def HashMap groupEvents
+	def ArrayList<User> members
+	def static final int MAX_EVENTS = 10
+	def int userType
+	
+	def boolean addEvent(Event NameEvent)
+	{
+		if(groupEvents.get(NameEvent.getIdEvent())== null && groupEvents.size() < MAX_EVENTS)
+		{
+			this.groupEvents.put(NameEvent.getIdEvent(), NameEvent);
+		    return true;
+		}else{
+		    return false;
+		}
+	}
+	
+	def boolean removeEvent(Event NameEvent)
+	{
+		if(groupEvents.get(NameEvent.getIdEvent()) != null){
+			this.groupEvents.remove(NameEvent.getIdEvent());
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public String toString(){
+		//String lol = "";
+		//for (Map.Entry<Integer, Event> entry : groupEvents.entrySet()) {
+			//lol += "Key = " + entry.getKey() + ", Value = " + entry.getValue();
+		//}
+		return "pecora";
+	}
+	
+	def void addUser(User NameUser)
+	{
+		members.add(NameUser)
+	}
+	
+	//Default Constructor
+	public Group()
+	{
+			groupEvents = new HashMap<Integer,Event>(MAX_EVENTS);
+	}
+	
+	//Default Constructor
+	public Group(int idGroup,String nameGroup,String description,Date createAt,
+		ArrayList<Event> groupEvents,ArrayList<User> members,int maxEvents,int userType)
+	{
+		super();
+		this.idGroup = idGroup;
+		this.nameGroup = nameGroup;
+		this.description = description;
+		this.createAt = createAt;
+		this.groupEvents = groupEvents;
+		this.members = members;
+		this.maxEvents = maxEvents;
+		this.userType = userType;
+		
+	}
+
+	
+	
 }
