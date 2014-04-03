f2da4d6f0a7f2fca81e3cfb9214322229a73cbd5
bones.felipe <bones.felipe@localhost>
2014-04-02 22:34:45 -0500

    Subject: Register Page done.

--- grails-app/conf/com/casualtyApp/model/SecSecurityFilters.groovy
+++ grails-app/conf/com/casualtyApp/model/SecSecurityFilters.groovy
@@ -5,9 +5,35 @@ package com.casualtyApp.model
  * via access control by convention.
  */
 class SecSecurityFilters {
+	
+	/*
+	 * Defines public pages to access, like the signup and auth
+	 * @author: Felipe
+	 */
+	def publicActions = [
+		signup: ['index','register'],
+		auth:['*']
+	]
+	
+	/*
+	 * If c does not exists returns false, if exists checks if the action
+	 * is allowed to be accessed, via find checking if the returned value is
+	 * null or not.
+	 */
+	private boolean findAction(controllerName, actionName){
+		def c = publicActions[controllerName]
+		return (c) ? c.find{ ( it==actionName||it=='*' ) } != null : false
+	}
+	
     def filters = {
         all(uri: "/**") {
             before = {
+				
+				//Check for public controller/actions
+				def isPublic=findAction(controllerName,actionName)
+				
+				if(isPublic) return true
+				
                 // Ignore direct views (e.g. the default main index page).
                 if (!controllerName) return true
 
