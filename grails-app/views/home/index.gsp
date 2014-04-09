63187d8774c843e14de9cd1146e4d062a4c0d4d8
bones.felipe <bones.felipe@localhost>
2014-04-08 21:22:44 -0500

    Map added to index page, maps.js created in order to save Google Maps Api related Javascript functions. home_index.css changed to fit map with the title. The map by the default uses geolocation to create the new map (Altough it may be removed).

--- grails-app/views/home/index.gsp
+++ grails-app/views/home/index.gsp
@@ -1,13 +1,33 @@
+
 <!DOCTYPE html>
+<!-- includes map javascript functions. -->
+
+<g:javascript src="maps.js" />
+
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <link rel="stylesheet" href="${resource(dir:'css',file:'home_index.css')}" />
     <title>Casualty</title>
+    
+    <!-- Housekeeping before insert the map div -->
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <style type="text/css">
+      html { height: 100% }
+      body { height: 100%; margin: 0; padding: 0 }
+      #map_canvas { height: 100% }
+    </style>
+    <!-- Call to google maps api -->
+    <script type="text/javascript"
+      src="http://maps.googleapis.com/maps/api/js?key=AIzaSyC5hrTO3Yea1wH40Oolq1YQCzSjPdZYedI&sensor=false">
+    </script>
+    
 </head>
-	<body>
-	
+	<!-- when the load is done initialize the map. -->
+	<body onload="initialize()" >
+		<!-- Puts the div map with full screen.-->
 		<h1>Welcome to CasualtyApp</h1>
-	
+		<div id="map_canvas" style="width:100%; height:100%"></div>
+		
 	</body>
 </html>
---
+++ web-app/js/maps.js
@@ -0,0 +1,54 @@
+/*
+ * Javascript functions to manipulate the
+ * google maps API.
+ * 
+ * @author: Felipe
+ */
+
+/*
+ * Function that is called by the GSP to init the map.
+ * First try to get geolocation via getCurrentPosition,
+ * if its successful passes do_initialize() as callback function,
+ * if fails a alert is show and default loader is called.
+ * 
+ */
+function initialize() {
+	
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(do_initialize);
+	  } else {
+	    alert("Geolocation is not supported by this browser.");
+		default_map_loader();
+	  }
+}
+/*
+ * Callback function to initalize the map, by default with zoom 13 
+ * and positions as parameters. It inits the map in the HTML finding the
+ * div with id "map_canvas".
+ */
+function do_initialize(position){
+	
+	var lat = position.coords.latitude;
+	var lng = position.coords.longitude;
+	
+	var mapOptions = {
+	          center: new google.maps.LatLng(lat, lng),
+	          zoom: 13,
+	          mapTypeId: google.maps.MapTypeId.ROADMAP
+	        };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        mapOptions);
+}
+
+/*
+ * Inits the map with no position as argument.
+ */
+function default_map_loader(){
+	var mapOptions = {
+	          center: new google.maps.LatLng(4.6442071,-74.100177),
+	          zoom: 12,
+	          mapTypeId: google.maps.MapTypeId.ROADMAP
+	        };
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+        mapOptions);
+}
