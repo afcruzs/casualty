254c73488a206cd1063fc01e0a40bac055c89d79
fabiankas <fabiankas@localhost>
2014-05-29 11:14:22 -0500

    Carga eventos sin importar el tam que tengan

---
+++ grails-app/conf/BootStrap.groovy
@@ -0,0 +1,73 @@
+import org.apache.shiro.crypto.hash.Sha512Hash
+
+import com.casualtyApp.model.ClassGroup
+import com.casualtyApp.model.Event
+import com.casualtyApp.model.EventCreator
+import com.casualtyApp.model.SecRole
+import com.casualtyApp.model.SecUser
+import com.casualtyApp.model.User
+
+class BootStrap {
+	
+	def shiroSecurityService
+	
+    def init = { servletContext ->
+		/*
+		 * Here users and roles are initalized according to Shiro's rules.
+		 * @author: Felipe
+		 */
+		def adminRole = new SecRole(name: "Administrator")
+		adminRole.addToPermissions("*:*")
+		adminRole.save()
+	   
+		def userRole = new SecRole(name:"User")
+		userRole.addToPermissions("Home:*")
+		/*userRole.addToPermissions("Home:index")
+		userRole.addToPermissions("Home:saveNewEvent")
+		userRole.addToPermissions("Signup:register")
+		userRole.addToPermissions("Home:profile")*/
+		userRole.save()
+	   
+		def admin = new SecUser(username: "Admin", passwordHash: new Sha512Hash("password").toHex())
+		admin.addToRoles(adminRole)
+		admin.save()
+	   
+		def a = new User(emailUser:'davanegaspr@unal.edu.co',createdAt: new Date(), isUnalConfirmed:true,shiroUser:admin, eventCreator : new EventCreator()
+			, name : "Diego", lastName : "Vanegas", biography: "La biografia", ubication : "San Andres")
+		a.save(flush:true,failOnError:true)
+		
+		def ev = new Event('Yoga en la UN ',new Date(),new Date(),'Relajate con Yoga en la UN, es gratis.',1,"Jugar,correr,yoga",4.6348317,-74.0820247)
+		a.eventCreator.addToEvents( ev )
+		a.eventCreator.addToEvents( new Event('Marcha a la 30',new Date(),new Date(),'Marcha en apoyo al paro agrario',1,"Marcha,campesinos",4.6356794,-74.0825153) )
+		a.eventCreator.addToEvents( new Event('Toque al aire libre.',new Date(),new Date(),'Toque al aire libre organizado por la facultad de artes.',1,"Toque, musica, arte",4.635862,-74.0826558) )
+		a.eventCreator.addToEvents( new Event('Dona un libro.',new Date(),new Date(),'Donacion de libros para jovenes de bachillerato, dona los libros que ya no uses.',1,"Educacion, libros",4.6352444,-74.082923) )
+		//a.addToClassGroup(new ClassGroup(nameGroup:'futball',description:'deportivo',new Date(),userType:2))
+		/*creacion de grupos
+		 * @author: Diego
+		 * */
+		def z = new User(emailUser:'ptorresg@unal.edu.co',createdAt: new Date(), isUnalConfirmed:true,shiroUser:admin, eventCreator : new EventCreator()
+			, name : "julian", lastName : "perez", biography: "estudiante...", ubication : "Bogota")
+		z.save(flush:true,failOnError:true)
+		
+		def ii = new ClassGroup(nameGroup:'Pecoras',description:'Viernes de juegos de azar',createAt: new Date(),userType:2,eventCreator : new EventCreator())
+		ii.save(flush:true,failOnError:true)
+		
+		ii.addToUser(z)
+		
+		//println e.getEventCreator().getUser()
+		
+		
+		def aa = new Event('Competencia de natacion',new Date(),new Date(),'vive la emocion de la competencia y gana muchos premios',2,"Nadar,saltar",4.6362093,-74.0830734)
+		ii.eventCreator.addToEvents(aa)
+					
+    }
+    def destroy = {
+    }
+}
+
+
+   
+   
+
+
+
---
+++ grails-app/controllers/com/casualtyApp/model/SignupController.groovy
@@ -0,0 +1,90 @@
+package com.casualtyApp.model
+
+import org.apache.shiro.authc.UsernamePasswordToken
+import org.apache.shiro.crypto.hash.Sha512Hash
+import org.apache.shiro.SecurityUtils
+
+
+class SignupController {
+
+    def index() {
+		SecUser user = new SecUser()
+		[user: user]
+	}
+	
+	def register(){
+		// Check to see if the username already exists
+		def user = SecUser.findByUsername(params.username)
+		if (user) {
+			flash.message = "User already exists '${params.username}'"
+			redirect(action:'index')
+		}
+		
+		// User doesn't exist with username. Let's create one
+		else {
+
+			// Make sure the passwords match
+			if (params.password != params.password2) {
+				flash.message = "Passwords do not match"
+				redirect(action:'index')
+			}
+
+			// Passwords match. Let's attempt to save the user
+			else {
+				// Create user
+				user = new SecUser(
+						username: params.username,
+						passwordHash: new Sha512Hash(params.password).toHex()
+				)
+
+				if (user.save()) {
+
+					// Add USER role to new user
+					def userRole =  SecRole.findByName('User')
+					user.addToRoles(userRole)
+					user.save()
+					
+					
+					//save user info in User instance
+					
+					def userInfo = new User(emailUser:params.email,createdAt: new Date(), 
+						isUnalConfirmed:isUnalConfirmed(params.email),shiroUser:user, 
+						eventCreator : new EventCreator(), name : params.name, 
+						lastName : params.lastname, biography: params.biography, 
+						ubication : params.ubication)
+					
+					userInfo.save()
+					
+					// Login user
+					def authToken = new UsernamePasswordToken(user.username, params.password)
+					SecurityUtils.subject.login(authToken)
+					
+					redirect(controller: 'home', action: 'index' )
+				}
+				else {
+					redirect(controller: 'auth', action: 'index')
+				}
+			}
+		}
+		
+	}
+	
+	def loginaux(){
+		redirect(controller: 'auth', action: "index", params: params)
+	}
+	
+	/*
+	 * Permite establecer si elcorreo suministrado
+	 * es unal.edu.co.
+	 * 
+	 * @author: Felipe
+	 */
+	def isUnalConfirmed(String email){
+		String [] t = email.split("@")
+		if( t.length < 2 ) return false
+		return t[1].equals("unal.edu.co")
+		
+	}
+	
+	
+}
