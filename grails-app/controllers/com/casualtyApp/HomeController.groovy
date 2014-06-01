7e34873e3dfcc192cb13b88158f1482909c6364d
uK yepe <uK yepe@localhost>
2014-05-31 23:35:18 -0500

    He instalado un plugin llamado richui-0.8 el cual ofrece diferentes funcionalidades parecidas a las de bootstrap con el objetivo de utilizar la funcionalidad de autocompletar, ademas he modificado el codigo css del plugin para que concordara con la aplicacion. He arreglado la barra de busqueda pero aun falta que lo mande al markert con el evento (nose si esa es la idea), ademas nose si seria mejor buscar por tags

---
+++ grails-app/controllers/com/casualtyApp/HomeController.groovy
@@ -0,0 +1,260 @@
+package com.casualtyApp
+
+import org.apache.shiro.SecurityUtils
+
+import com.casualtyApp.model.Event
+import com.casualtyApp.model.SecUser;
+import com.casualtyApp.model.User;
+
+import grails.converters.JSON
+
+class HomeController {
+	
+	def eventsService
+	
+	def index() {
+	
+	   render( view: "index", model : [ events: eventsService.getFirstEvents(), username :
+		   SecurityUtils.getSubject().getPrincipal() ] )
+	}
+
+	def admin() {
+	   //render "This page requires the logged in user to be an Administrator"
+	}
+	
+	/*
+	 * Se salva el nuevo evento en la base de datos relacionandolo con
+	 * el usuario actualmente logueado.
+	 *
+	 * @author: Felipe
+	 */
+	def saveNewEvent(){
+		try{
+			def currentUser = User.get( SecUser.findByUsername(SecurityUtils.getSubject().getPrincipal()).id )
+			
+			
+			
+			Event newEvent =  new Event(params.title,parseDate(params.startTime, params.startHour),
+													parseDate(params.endTime, params.endHour),params.description,1, params.tags,
+													Double.parseDouble(params.latitude),
+													Double.parseDouble(params.longitude)) 
+			
+			newEvent.addToAssistants(currentUser)
+			currentUser.addToEventsToAttend(newEvent)
+			currentUser.eventCreator.addToEvents( newEvent )
+			
+		}catch(Exception e){
+			render "Error"
+		}
+		render "Success"
+		
+	}
+	
+	/*
+	 * Funcion que sirve para agregar un usuario a la lista de personas que asistirán a un evento
+	 * 
+	 */
+	
+	def attendEvent(){
+		
+		try{
+			def eventForAttend = Event.get(params.idevent)
+			
+						
+			def currentUser = User.get( SecUser.findByUsername(SecurityUtils.getSubject().getPrincipal()).id )			
+			eventForAttend.addToAssistants(currentUser)
+			currentUser.addToEventsToAttend(eventForAttend)
+			render "Success"
+		}
+		catch(Exception e){
+			render "Error"
+		}
+		
+	}
+	
+	
+	
+	def isAssistant(){
+		try{
+		def eventForAttend = Event.get(params.idevent)	
+		def currentUser = User.get( SecUser.findByUsername(SecurityUtils.getSubject().getPrincipal()).id )			
+		if(currentUser in eventForAttend.assistants){
+			render "Yes"
+			System.out.println("Es un asistente")
+			}
+		else{
+			render "No"
+			}
+		}
+		catch(Exception e){
+			render "Error"
+		}
+		
+	}
+	
+	def unAttendEvent(){
+		try{
+		def currentUser = User.get( SecUser.findByUsername(SecurityUtils.getSubject().getPrincipal()).id )
+		def eventForAttend = Event.get(params.idevent)
+		eventForAttend.removeFromAssistants(currentUser)
+		currentUser.removeFromEventsToAttend(eventForAttend)
+		render "Success"
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			render "Error"
+		}
+		
+	}
+	
+	/*
+	 * Funcion auxiliar para convertir el string que retorna la vista en un Date
+	 */
+	def parseDate(String d,String hour){
+		
+		def t = d.split("-")
+		int year = Integer.parseInt(t[0])
+		int month = Integer.parseInt(t[1])-1
+		int day = Integer.parseInt(t[2])
+		
+		def h = hour.split(":")
+		int _hour = Integer.parseInt( h[0] )
+		int minute = Integer.parseInt( h[1] )
+		
+		Calendar date = Calendar.getInstance();
+		date.set(Calendar.YEAR, year);
+		date.set(Calendar.MONTH, month);
+		date.set(Calendar.DAY_OF_MONTH, day);
+		
+		
+		date.set(Calendar.HOUR_OF_DAY, _hour)
+		date.set(Calendar.MINUTE, minute)
+		
+		
+		
+		return date.getTime()
+
+		
+	}
+	
+	
+	/*
+	 * Obtiene los �ltimos eventos en la DB que sean mayores
+	 * al id que se pasa por parametro, estos eventos son convertidos
+	 * al json est�ndar que se usa en el servide y se devuelven via
+	 * Ajax con el render, para que posteriormente en javascript puedan
+	 * ser procesados y le�dos por el mapa.
+	 *
+	 * @author: felipe
+	 */
+	def getLastEvents(){
+		def lastEvents = Event.findAllByIdGreaterThan(params.lastEventId)
+		def jsonEvents = []
+		for( Event e : lastEvents)
+			jsonEvents.add( eventsService.eventToJSON(e) )
+		
+		render jsonEvents as JSON
+	}
+	
+	
+	/*def getEvents(){
+		def events = Event.findAll()
+		def jsonEvents = []
+		for( Event e : events){
+			
+			jsonEvents.add( eventsService.eventToJSON(e) )
+		}
+		render jsonEvents as JSON	
+	 }*/
+	
+	/* Obtiene todos los eventos por nombre para autocompletar en el buscador utilizando
+	 * el plugin richui-0.8
+	 */
+	
+	def searchAJAX = {
+		def events = Event.findAllByTitleLike("%${params.query}%")
+
+		//Create XML response
+		render(contentType: "text/xml") {
+		results() {
+			events.each { event ->
+			result(){
+					name(event.title)
+					}
+				}
+			}
+		}
+	}
+	
+	
+	def changePassword(){
+		println ("asdsaeloo")
+		redirect(controller: 'home', action: 'index' )
+		
+		
+	}
+	
+	
+	def profile(){
+		def currentUser = User.get( SecUser.findByUsername(SecurityUtils.getSubject().getPrincipal()).id )
+		def nombresEvent="";
+		def descEvent="";
+		for(event in currentUser.eventCreator.events){
+			 descEvent=descEvent + event.description + "@"
+			 nombresEvent=nombresEvent + event.title + "@"
+			 
+		 }
+		
+		render( view: "profile",model : [user : currentUser , username :
+		   SecurityUtils.getSubject().getPrincipal() , names : nombresEvent , desc : descEvent , tam : currentUser.eventCreator.events.size()] )
+		
+	}
+	
+	
+	
+	
+	def publicProfile(){
+		def username = params.username
+		
+		if( SecUser.findByUsername(username) != null ){
+			def currentUser = User.get( SecUser.findByUsername(username).id )
+			
+			def namesEvent="";
+			def descEvent="";
+			for(event in currentUser.eventCreator.events){
+				 descEvent=descEvent + event.description + "@"
+				 namesEvent=namesEvent + event.title + "@"
+				 
+			 }
+			
+			render( view: "publicProfile",model : [user : currentUser , username :
+				username , names : namesEvent , desc : descEvent , tam : currentUser.eventCreator.events.size()] )
+		}else{
+			render( view: "publicGroup",model : [ groupName : username ] )
+			
+		}
+	}
+	
+	def publicGroup(){
+
+	}
+	
+	def updateProfile(){
+		def currentUser = User.get( SecUser.findByUsername(SecurityUtils.getSubject().getPrincipal()).id )
+		
+		currentUser.biography=params.biography
+		currentUser.name=params.name
+		currentUser.lastName=params.lastName
+		currentUser.ubication=params.ubication
+		currentUser.emailUser=params.email
+		
+		if(currentUser.save()){
+			redirect(controller: 'home', action: 'index' )
+		}else{
+			redirect(controller: 'home', action: 'index' )
+		}
+		
+		
+	}
+	
+}
