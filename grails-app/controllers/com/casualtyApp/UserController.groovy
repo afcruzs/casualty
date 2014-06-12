7c86cb8e9084fc66668d5e353407bf5e04f08bd4
bones.felipe <bones.felipe@localhost>
2014-06-12 02:44:00 -0500

    Funcionalidad de la barra de b?squeda implementada, cuando selecciona el item lo envia a la pagina de public group, aunque se ve esteticamente horrible.

---
+++ grails-app/controllers/com/casualtyApp/UserController.groovy
@@ -0,0 +1,36 @@
+package com.casualtyApp
+
+import com.casualtyApp.model.User
+
+
+/*
+ * 	This controller handles CRUD operations on Users.
+ */
+
+class UserController {
+	
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+	def index() { }
+	
+	def create() { 
+		
+		render "Creating"
+		/*def newUser = new User()
+		newUser.properties = params
+		newUser.save()
+		return [userInstance: newUser]*/
+		
+	}
+	
+	def read() { }
+	
+	def update() { }
+	
+	def delete() { }
+	
+	//Shows content
+	def list(){
+		render User.getAll()
+	}
+}
---
+++ grails-app/domain/com/casualtyApp/model/ClassGroup.groovy
@@ -0,0 +1,37 @@
+package com.casualtyApp.model
+
+class ClassGroup{
+	/*se eliminaron atributos redundantes y tablas que el el Gorm ya crea por debajo,
+	 * se eliminaron los constructores ya que su funcion de ingreso de datos tambien es construido por el GORM
+	 * @author: Diego
+	 */
+	def String nameGroup
+	def String description
+	def Date createAt
+	def int userType
+	def byte[] screenshot;
+	def EventCreator eventCreator
+	
+	static belongsTo = User
+	static hasMany = [user:User] //un grupo puede crear cero o muchos eventos, un grupo puede contener minimo un o muchos usuarios
+	
+	/*ninguno de los atributos se permite que sean nulos, no sean espacios en blanco
+	 * y que ninguno sea unico excepto por el nombre del grupo.
+	 * @author: Diego
+	 * */
+	static constraints = {
+		nameGroup (nullable: false, blank: false, unique: true, maxSize:300)
+		eventCreator(nullable: true)
+		description(nullable: false, blank: false,unique: false,maxSize:800)
+		   createAt(nullable: false, blank: false, unique: false)
+		   userType(nullable: false, blank: false, unique: false)
+		   screenshot(maxSize:1073741824)
+		   screenshot(nullable:true)
+	}
+	
+		
+	public String toString(){
+		return "pecora";
+	}
+		
+}
